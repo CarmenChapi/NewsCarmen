447021a8b392c2f6be68c76336f52756098842fa
CarmenChapi <mariachaparro58@gmail.com>
2024-08-29 22:06:02 +0100

    add feature-11

--- db/queries.js
+++ db/queries.js
@@ -12,8 +12,8 @@ COUNT(comments.comment_id) AS comment_count
 FROM articles 
 LEFT JOIN comments ON 
 articles.article_id = comments.article_id 
-GROUP BY articles.article_id
-ORDER BY articles.created_at DESC;`;
+GROUP BY articles.article_id `;
+
 
 
 
