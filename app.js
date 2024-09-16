654dd837d57c017b9d0dbca87cbbc2ead83e5457
CarmenChapi <mariachaparro58@gmail.com>
2024-09-16 10:27:06 +0100

    allow cors origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 const { getAllEndpoints, postEndpoint} = require("./controllers/endpoints")
 const {getAllTopics} = require("./controllers/topics")
